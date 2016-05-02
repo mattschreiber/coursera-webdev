7e0b815115dc73a1212711aab9bb1693d4c8ee7c
Matt S <Matt S>
2016-05-02 15:50:07 -0400

    attemps to fix collapseable nav when I click link from desktop browser (window must be less then 768px)

--- site/mod3_solution/js/script.js
+++ site/mod3_solution/js/script.js
@@ -1,3 +1,7 @@
+// Function to close navbartoggle menu when it loses focus
+// This doesn't seem to work write when used on desktop browser with screen width less then 768.  
+// The behaviour is sporadic, but clicking on Sushi (or whatever is bottom link) doesn't do anything.  The other links usuall work.
+
 $(function () {
 	 $("#navbarToggle").blur(function(event) {
 	 	var screenWidth = window.innerWidth;
@@ -8,3 +12,13 @@ $(function () {
 	 });
 });
 
+//This seems to work better, but it doesn't take me to the exact location of the element specified in the href
+// $(function () {
+//     $(document).click(function (event) {
+//         var clickover = $(event.target);
+// 		var _opened = $("#collapsable-nav").hasClass("navbar-collapse collapse in");
+//         if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+//             $("#collapsable-nav").collapse("hide");
+//         }
+//     });
+// });
